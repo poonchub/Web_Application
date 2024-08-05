6a7abab3d495f33ccea36b82f12cb6191e0cd203
poonchub <141898614+poonchub@users.noreply.github.com>
2024-08-05 15:29:13 +0700

    5/29/67

---
+++ Week04/thank.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>thank</title>
+    </head>
+    <body style="
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding-top: 100px;
+    ">
+        <div style="
+            border: 5px solid;
+            width: 70%;
+            height: 170px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        ">  
+            <?php
+                echo "Thank You Vary Much !";
+            ?>
+        </div>
+    </body>
+</html>
